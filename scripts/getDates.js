3a699e2846466a34f66c1d3d7c2ede819a4e7467
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-09-12 01:39:54 -0300

    js complete

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el año actual y asignarlo al span dentro del primer párrafo del footer
+    
     const currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
 
-    // Asignar la fecha de la última modificación al segundo párrafo del footer
+    
     const lastModifiedDate = document.lastModified;
     document.getElementById("lastModifiedDate").textContent = "Last Modification: " + lastModifiedDate;
 });
