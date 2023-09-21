4943f22c4503fd78cb79cf135220a0ea331bc5e7
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-09-20 23:33:42 -0300

    Next assignmet

---
+++ chamber/scripts/bigHero.js
@@ -0,0 +1,19 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.slide-image');
+const nextButton = document.getElementById('nextSlide');
+const prevButton = document.getElementById('prevSlide');
+
+nextButton.addEventListener('click', function() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+});
+
+prevButton.addEventListener('click', function() {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    images[currentImageIndex].style.display = 'block';
+});
+
+// Mostrar la primera imagen al inicio
+images[0].style.display = 'block';
--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -5,3 +5,10 @@ hambutton.addEventListener('click', () => {
   mainnav.classList.toggle('show');
   hambutton.classList.toggle('show');
 });
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth >= 768) {
+      mainnav.classList.remove('show'); // Asegura que el menú esté visible
+      hambutton.classList.remove('show'); // Oculta el botón de hamburguesa
+  }
+});
