d18ffee145affc239406fdcdb0f316ed9bba7d73
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-10-07 01:46:33 -0300

    visitMessage

--- chamber/scripts/daysBetweenVisits.js
+++ chamber/scripts/daysBetweenVisits.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const visitMessage = document.getElementById('visitMessage');
+    const oneDay = 84600000; 
     const currentDate = Date.now();
-    const oneDay = 24 * 60 * 60 * 1000; 
     const lastVisitDate = localStorage.getItem('lastVisitDate');
     
     if (!lastVisitDate) {
-        visitMessage.textContent = "Welcome for the first time!";;
+        visitMessage.textContent = "Welcome! Let us know if you have any questions.";;
     } else {
         const daysSinceLastVisit = Math.round((currentDate - lastVisitDate) / oneDay);
 
         if (daysSinceLastVisit < 1) {
-            visitMessage.textContent = "You visited today. Welcome back!";
+            visitMessage.textContent = "Back so soon! Awesome!";
         } else if (daysSinceLastVisit === 1) {
             visitMessage.textContent = "You last visited 1 day ago.";
         } else {
