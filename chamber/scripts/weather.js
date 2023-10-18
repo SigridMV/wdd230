6ecb935882d11bbdd06a160bb2c91b53c19db172
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-10-18 16:27:11 -0300

    ok weather

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -7,41 +7,42 @@ async function fetchWeatherData(city) {
   try {
     const response = await fetch(endpoint);
     if (!response.ok) throw new Error('Network response was not ok');
-    return await response.json();
+    const data = await response.json();
+    if (!data || !data.main) throw new Error(`Invalid data for city: ${city}`);
+    return data;
   } catch (error) {
-    console.log("There was a problem with the fetch operation:", error.message);
+    console.error("There was a problem with the fetch operation:", error.message);
+    return null;  
   }
 }
 
 function displayCityWeather(city, data) {
+    if (!data) return;  
+    
     const weatherBoxes = document.querySelectorAll('.weather-box');
   
     for (const box of weatherBoxes) {
       if (box.querySelector('.city-name').textContent.trim() === city) {
-        
         const temp = box.querySelector('.temp');
         temp.innerHTML = `${data.main.temp}°C`;
-  
-       
         const descSpan = box.querySelector('.desc');
         let desc = data.weather[0].description;
         descSpan.textContent = desc;
-  
-        
         const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
         const weatherIcon = box.querySelector('.weather-icon');
         weatherIcon.setAttribute("src", iconSrc);
         weatherIcon.setAttribute("alt", desc);
-  
         break;
       }
     }
-  }
+}
 
 async function initWeather() {
   for (const city of cities) {
     const data = await fetchWeatherData(city);
-    displayCityWeather(city, data);
+    if (data) {  
+      displayCityWeather(city, data);
+    }
   }
 }
 
