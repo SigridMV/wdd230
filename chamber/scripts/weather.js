f3ad7867dc88da4101e5d464c8b4a917da100c5e
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2023-10-18 23:37:15 -0300

    weather-box ok

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,48 +1,75 @@
 const apiKey = 'd13ca374f2f79abbac373e89e9c0210e';
 const cities = ['London', 'Santiago', 'Dubai', 'New York', 'Madrid'];
 
-async function fetchWeatherData(city) {
+async function fetchCurrentWeather(city) {
   const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
   try {
     const response = await fetch(endpoint);
     if (!response.ok) throw new Error('Network response was not ok');
-    const data = await response.json();
-    if (!data || !data.main) throw new Error(`Invalid data for city: ${city}`);
-    return data;
+    return await response.json();
   } catch (error) {
-    console.error("There was a problem with the fetch operation:", error.message);
+    console.error(`Error fetching current weather for ${city}:`, error.message);
     return null;  
   }
 }
 
-function displayCityWeather(city, data) {
-    if (!data) return;  
-    
-    const weatherBoxes = document.querySelectorAll('.weather-box');
-  
-    for (const box of weatherBoxes) {
-      if (box.querySelector('.city-name').textContent.trim() === city) {
-        const temp = box.querySelector('.temp');
-        temp.innerHTML = `${data.main.temp}°C`;
-        const descSpan = box.querySelector('.desc');
-        let desc = data.weather[0].description;
-        descSpan.textContent = desc;
-        const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        const weatherIcon = box.querySelector('.weather-icon');
-        weatherIcon.setAttribute("src", iconSrc);
-        weatherIcon.setAttribute("alt", desc);
-        break;
+async function fetchThreeDayForecast(city) {
+  const endpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&cnt=24`;
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) throw new Error('Network response was not ok');
+    const data = await response.json();
+    return data.list.filter((forecast, index) => index % 8 === 0); 
+  } catch (error) {
+    console.error(`Error fetching forecast for ${city}:`, error.message);
+    return null;
+  }
+}
+
+async function displayCityWeather(city) {
+  const currentData = await fetchCurrentWeather(city);
+  const forecastData = await fetchThreeDayForecast(city);
+
+  const weatherBox = [...document.querySelectorAll('.weather-box')].find(
+    box => box.querySelector('.city-name').textContent.trim() === city
+  );
+
+  const icon = weatherBox.querySelector('.weather-icon');
+  if (currentData && currentData.weather && currentData.weather[0]) {
+      icon.src = `https://openweathermap.org/img/w/${currentData.weather[0].icon}.png`;
+      icon.alt = currentData.weather[0].description;
+      icon.classList.remove('default-icon');
+  } else {
+      icon.classList.add('default-icon');  
+  }
+
+  if (currentData) {
+      const currentDate = new Date(currentData.dt * 1000);
+      const formattedDate = `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
+      const temp = weatherBox.querySelector('.temp');
+      temp.innerHTML = `${formattedDate}<br>${currentData.main.temp}°C`;
+
+      const descSpan = weatherBox.querySelector('.desc');
+      descSpan.innerHTML = `${currentData.weather[0].description}<br>`;
+  }
+
+  if (forecastData) {
+      for (let i = 0; i < 2; i++) {
+          const forecast = forecastData[i];
+          if (forecast) {
+              const forecastDate = new Date(forecast.dt * 1000);
+              const formattedForecastDate = `${forecastDate.getDate()}-${forecastDate.getMonth() + 1}-${forecastDate.getFullYear()}`;
+              const forecastTemp = document.createElement('p');
+              forecastTemp.innerHTML = `${formattedForecastDate}<br>${forecast.main.temp}°C`;
+              weatherBox.appendChild(forecastTemp);
+          }
       }
-    }
+  }
 }
 
 async function initWeather() {
   for (const city of cities) {
-    const data = await fetchWeatherData(city);
-    if (data) {  
-      displayCityWeather(city, data);
-    }
+    await displayCityWeather(city);  
   }
 }
 
